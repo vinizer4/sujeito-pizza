9a5ca787f37fa645fae1a3357e727106f397ade1
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-01-19 22:39:20 -0300

    feat: created details user route and middlewares isAuthenticated

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
@@ -11,5 +13,8 @@ router.post("/users", new CreateUserController().handle);
 
 // User Login
 router.post("/session", new AuthUserController().handle);
+
+// User Details
+router.get("/me", isAuthenticated, new DetailUserController().handle);
 
 export { router };
