c353c45996783c453e5093cd2c1414da0af92d04
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-01-19 23:01:53 -0300

    feat: created category POST route implemented

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,10 +3,11 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 
 const router = Router();
 
-// -- ROTAS USER --
+// -- ROUTES USER --
 
 // Create User
 router.post("/users", new CreateUserController().handle);
@@ -16,5 +17,14 @@ router.post("/session", new AuthUserController().handle);
 
 // User Details
 router.get("/me", isAuthenticated, new DetailUserController().handle);
+
+// -- ROUTES CATEGORY --
+
+// Create Category
+router.post(
+  "/category",
+  isAuthenticated,
+  new CreateCategoryController().handle
+);
 
 export { router };
