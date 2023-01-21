def99aa9bbbc187a4b9f9052cc969f583720b356
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-01-21 16:15:45 -0300

    feat: created login screen

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,35 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/home.module.scss";
+
+import logoImg from "../../public/logo.svg";
+
+import { Input } from "./components/ui/Input";
+import { Button } from "./components/ui/Button";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello Next</h1>
-    </div>
+    <>
+      <Head>
+        <title>SujeitoPizza - Faça seu login</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
+
+        <div className={styles.login}>
+          <form>
+            <Input placeholder="Digite seu email" type="text" />
+
+            <Input placeholder="Sua senha" type="password" />
+
+            <Button type="submit" loading={false}>
+              Acessar
+            </Button>
+          </form>
+
+          <a className={styles.text}>Nao possui uma conta? Cadastre-se</a>
+        </div>
+      </div>
+    </>
   );
 }
