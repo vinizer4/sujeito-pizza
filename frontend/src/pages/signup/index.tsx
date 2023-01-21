7f125ec464f7c69b9dbd34919f3f6e4d9a6cc8ce
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-01-21 16:36:31 -0300

    feat: created signup page

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,25 +1,28 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../../styles/home.module.scss";
+import styles from "../../../styles/home.module.scss";
 
-import logoImg from "../../public/logo.svg";
+import logoImg from "../../../public/logo.svg";
 
 import { Input } from "@/components/ui/Input";
 import { Button } from "@/components/ui/Button";
 
 import Link from "next/link";
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
       <Head>
-        <title>SujeitoPizza - Faça seu login</title>
+        <title>Faça seu cadastro agora!</title>
       </Head>
 
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo Sujeito Pizzaria" className="image" />
         <div className={styles.login}>
+          <h1>Crie sua conta</h1>
           <form>
+            <Input placeholder="Digite seu nome" type="text" />
+
             <Input placeholder="Digite seu email" type="text" />
 
             <Input placeholder="Sua senha" type="password" />
