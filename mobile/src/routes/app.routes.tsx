73b24fe45f6faf2131faec342e4458aa7024f2a0
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-01-22 19:18:04 -0300

    feat: implemented order page

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,16 +1,36 @@
-import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Dashboard from "../pages/Dashboard";
+import Dashboard from '../pages/Dashboard';
+import Order from '../pages/Order'
 
-const Stack = createNativeStackNavigator();
+export type StackPramsList = {
+  Dashboard: undefined;
+  Order: {
+    number: number | string;
+    order_id: string;
+  };
+};
 
-function AppRoutes() {
-  return (
+const Stack = createNativeStackNavigator<StackPramsList>();
+
+function AppRoutes(){
+  return(
     <Stack.Navigator>
-      <Stack.Screen name="Dashboard" component={Dashboard} />
+      <Stack.Screen 
+        name="Dashboard" 
+        component={Dashboard} 
+        options={{ headerShown: false }}
+      />
+
+      <Stack.Screen
+        name="Order"
+        component={Order}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
-  );
+  )
 }
 
 export default AppRoutes;
+
